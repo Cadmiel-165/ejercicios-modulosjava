09643332add73f10cb22f8c2990e99a775053578
Cadmiel-165 <25680165@cuautla.tecnm.mx>
2025-12-05 23:40:27 -0600

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,45 @@
+package Modulos.Ejercicio2;
+
+public class Producto {
+
+    // Atributos privados
+    private String referencia;
+    private int cantidad;
+    private double precioUnitario;
+
+    // Constructor
+    public Producto(String referencia, int cantidad, double precioUnitario) {
+        this.referencia = referencia;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+    // Getters y Setters
+    public String getReferencia() {
+        return referencia;
+    }
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    // Comportamiento 1, suma mas piezas al stock
+    public void aumentarStock(int cantidadEntrada) {
+        this.cantidad = this.cantidad + cantidadEntrada;
+    }
+
+    // Comportamiento 2, calcula cuanto vale todo el stock
+    public double valorTotal() {
+        return this.cantidad * this.precioUnitario;
+    }
+}
